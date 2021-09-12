6a4af763ccd48dc0758333c4b34e94a3b105231f
Nguyen Ngoc Truong <ngoctruong472k@gmail.com>
2021-09-12 23:20:26 +0700

    Update date 12/09

--- public/js/vong1.js
+++ public/js/vong1.js
@@ -1,4 +1,25 @@
 // vòng 1 *******************************************
+let coins = 100;
+let originalCoin = document.getElementById('coins');
+let coin = document.querySelector('.coinAnimation');
+console.log(coin);
+console.log(originalCoin);
+let pickUpCoin = () => {
+  coins += 20;
+  originalCoin.textContent = coins;
+  coin.style.display = "none";
+}
+let randomNumber = (min, max) => {
+  return Math.random() * (max - min) + min;
+}
+
+setInterval(() => {
+  setTimeout(() => {
+    coin.style.left = randomNumber(200, 1140) + "px";
+    coin.style.display = "block";
+
+  }, 10001);
+}, 10000);
 class ProgressRing extends HTMLElement {
   constructor() {
     super();
@@ -14,11 +35,11 @@ class ProgressRing extends HTMLElement {
                                   width="${radius * 2}"
                               >
                                   <circle
-                                  stroke="brown"
+                                  stroke="red"
                                   stroke-dasharray="${this._circumference} ${this._circumference}"
                                   style="stroke-dashoffset:${this._circumference}"
                                   stroke-width="${stroke}"
-                                  fill="bisque"
+                                  fill="white"
                                   r="${normalizedRadius}"
                                   cx="${radius}"
                                   cy="${radius}"
@@ -57,34 +78,62 @@ class ProgressRing extends HTMLElement {
 window.customElements.define('progress-ring', ProgressRing);
 
 let progress = 100;
+dogFeel = document.querySelector('#dogFeel');
+pet = document.querySelector('.pet');
 const el = document.querySelector('#progressRing');
-
 el.setAttribute('progress', progress);
 const interval = setInterval(() => {
   if (progress <= 0) {
     progress = 0;
     alert("Thú cưng của bạn đã chết, hãy bắt đầu lại!!");
   } else {
-    progress -= 10;
+    progress -= 20;
   }
   if (progress < 0) {
     return;
   }
   console.log(progress);
+  if (progress < 50) {
+    dogFeel.src = "public/images/hungryPet.gif";
+    pet.className = "petFeel";
+  }
+  if (progress >= 50) {
+    dogFeel.src = "public/images/tam.gif";
+    pet.className = "pet";
+  }
   el.setAttribute('progress', progress);
 }, 7000);
-
+let toTA = document.getElementById('toTA');
 const eatOne = document.querySelector('#eat-one');
 eatOne.addEventListener('click', e => {
-  if (progress < 100) {
-    progress += 20;
-  } else {
-    return
+  if (coins >= 20) {
+    if (progress < 100) {
+      coins -= 10;
+      progress += 20;
+      replenishReversal1();
+      setTimeout(() => {
+        progress += 20;
+        replenishReversal2();
+      }, 10000);
+      originalCoin.textContent = coins;
+    } else {
+      return
+    }
+    if (progress > 100) progress = 100;
+    console.log(progress);
+    el.setAttribute('progress', progress);
   }
-  if (progress > 100) progress = 100;
-  console.log(progress);
-  el.setAttribute('progress', progress);
+  else {
+    alert('Bạn không đủ tiền!')
+  }
 })
+
+let replenishReversal1 = () => {
+  toTA.src = "public/images/dodungtas-removebg.png";
+};
+let replenishReversal2 = () => {
+  toTA.src = "public/images/trong-removebg-preview.png";
+};
 
 const eatXuong = document.querySelector('#eat-xuong');
 eatXuong.addEventListener('click', e => {
@@ -128,11 +177,11 @@ class ProgressRing2 extends HTMLElement {
                                 >
                                     <circle
                                     id ="circle-two"
-                                    stroke="brown"
+                                    stroke="green"
                                     stroke-dasharray="${this._circumference} ${this._circumference}"
                                     style="stroke-dashoffset:${this._circumference}"
                                     stroke-width="${stroke2}"
-                                    fill="bisque"
+                                    fill="white"
                                     r="${normalizedRadius2}"
                                     cx="${radius2}"
                                     cy="${radius2}"
@@ -179,7 +228,7 @@ const interval2 = setInterval(() => {
   if (progress2 <= 0) {
     progress2 = 0;
   } else {
-    progress2 -= 10;
+    progress2 -= 5;
   }
   if (progress2 < 0) return;
   el2.setAttribute('progress2', progress2);
@@ -210,28 +259,28 @@ bear.addEventListener('click', e => {
 })
 
 
-  // vong 3 ************************************************************
-  class ProgressRing3 extends HTMLElement {
-    constructor() {
-        super();
-        const stroke3 = this.getAttribute('stroke');
-        const radius3 = this.getAttribute('radius');
-        const normalizedRadius3 = radius3 - stroke3 * 2;
-        this._circumference = normalizedRadius3 * 2 * Math.PI;
+// vong 3 ************************************************************
+class ProgressRing3 extends HTMLElement {
+  constructor() {
+    super();
+    const stroke3 = this.getAttribute('stroke');
+    const radius3 = this.getAttribute('radius');
+    const normalizedRadius3 = radius3 - stroke3 * 2;
+    this._circumference = normalizedRadius3 * 2 * Math.PI;
 
-        this._root = this.attachShadow({ mode: 'open' });
-        this._root.innerHTML = `
+    this._root = this.attachShadow({ mode: 'open' });
+    this._root.innerHTML = `
                                 <svg
                                     height="${radius3 * 2}"
                                     width="${radius3 * 2}"
                                 >
                                     <circle
                                     id ="circle-three"
-                                    stroke="brown"
+                                    stroke="aqua"
                                     stroke-dasharray="${this._circumference} ${this._circumference}"
                                     style="stroke-dashoffset:${this._circumference}"
                                     stroke-width="${stroke3}"
-                                    fill="bisque"
+                                    fill="white"
                                     r="${normalizedRadius3}"
                                     cx="${radius3}"
                                     cy="${radius3}"
@@ -246,25 +295,25 @@ bear.addEventListener('click', e => {
                                     }
                                 </style>
                                 `;
-    } // end constructor
+  } // end constructor
 
-    setProgress3(percent3) {
-        const offset3 = this._circumference - (percent3 / 100 * this._circumference);
-        const circle3 = this._root.querySelector('#circle-three');
-        circle3.style.strokeDashoffset = offset3;
+  setProgress3(percent3) {
+    const offset3 = this._circumference - (percent3 / 100 * this._circumference);
+    const circle3 = this._root.querySelector('#circle-three');
+    circle3.style.strokeDashoffset = offset3;
+  }
+
+
+
+  static get observedAttributes() {
+    return ['progress3'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'progress3') {
+      this.setProgress3(newValue);
     }
-
-
-
-    static get observedAttributes() {
-        return ['progress3'];
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-        if (name === 'progress3') {
-            this.setProgress3(newValue);
-        }
-    }
+  }
 }
 
 window.customElements.define('progress-ring3', ProgressRing3);
@@ -275,22 +324,22 @@ const el3 = document.querySelector('#progressRing3');
 
 el3.setAttribute('progress3', progress3);
 const interval3 = setInterval(() => {
-    if (progress3 <= 0) {
-        progress3 = 0;
-    } else {
-        progress3 -= 10;
-    }
-    if (progress3 < 0) return;
-    el3.setAttribute('progress3', progress3);
+  if (progress3 <= 0) {
+    progress3 = 0;
+  } else {
+    progress3 -= 5;
+  }
+  if (progress3 < 0) return;
+  el3.setAttribute('progress3', progress3);
 }, 9000);
 
 const clearBody = document.querySelector('#clear-body');
 clearBody.addEventListener('click', e => {
-    if (progress3 < 100) {
-      progress3 += 30;
-    } else {
-      return
-    }
-    if (progress3 > 100) progress3 = 100;
-    el3.setAttribute('progress3', progress3);
+  if (progress3 < 100) {
+    progress3 += 30;
+  } else {
+    return
+  }
+  if (progress3 > 100) progress3 = 100;
+  el3.setAttribute('progress3', progress3);
 })
